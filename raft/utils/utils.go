3b480039fc4a5dc2941ef0e5524ae485e8c7146e
Kirill Kondrenko <kondrenkokp@yandex.ru>
2025-03-06 13:44:10 +0700

    wait, what

--- raft/utils/utils.go
+++ raft/utils/utils.go
@@ -4,6 +4,7 @@ import "distributed-algorithms/raft/context"
 
 func CheckTerm(ctx *context.Context, term int32) {
 	if term > ctx.GetCurrentTerm() {
-
+		ctx.SetCurrentTerm(term)
+		ctx.BecomeFollower()
 	}
 }
