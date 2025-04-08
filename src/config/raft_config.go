921586b1f637df02ae5e35b6fc665e933a9f513d
Kirill Kondrenko <kondrenkokp@yandex.ru>
2025-04-08 13:54:56 +0700

    ooops

--- src/config/raft_config.go
+++ src/config/raft_config.go
@@ -4,7 +4,6 @@ type RaftConfig struct {
 	BroadcastTimeMs      int `env:"BROADCAST_TIME_MS,required"`
 	MinElectionTimeoutMs int `env:"MIN_ELECTION_TIMEOUT_MS,required"`
 	MaxElectionTimeoutMs int `env:"MAX_ELECTION_TIMEOUT_MS,required"`
-	HealthCheckTimeoutMs int `env:"HEALTH_CHECK_TIMEOUT_MS,required"`
 	SelfNode             struct {
 		Address string `env:"SELF_ADDRESS,required"`
 		Id      string `env:"SELF_ID,required"`
